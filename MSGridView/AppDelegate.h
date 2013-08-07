e3caa4a7412dbc92901d239d9e13245898d19e9e
Martin Saunders <martin.saunders@tbc-digital.com>
2013-08-07 09:31:19 +0100

    Initial Commit

---
+++ MSGridView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MSGridView
+//
+//  Created by Martin Saunders on 07/08/2013.
+//  Copyright (c) 2013 Martin Saunders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MSGridView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MSGridView
+//
+//  Created by Martin Saunders on 07/08/2013.
+//  Copyright (c) 2013 Martin Saunders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
